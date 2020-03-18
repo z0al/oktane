c99fe2a964a1f07178bb387ba527b2ab1e482a9f
Ahmed T. Ali <ah.tajelsir@gmail.com>
2020-03-18 08:55:26 +0100

    refactor(core): add state reducer

---
+++ src/internals/state.test.ts
@@ -0,0 +1,168 @@
+// Ours
+import { transition } from './state';
+
+const TESTS = [
+	// idle
+	{
+		current: 'idle',
+		operation: { type: 'fetch' },
+		expected: 'pending',
+	},
+	{
+		current: 'idle',
+		operation: { type: 'cancel' },
+		expected: 'idle',
+	},
+	{
+		current: 'idle',
+		operation: { type: 'reject' },
+		expected: 'idle',
+	},
+	{
+		current: 'idle',
+		operation: { type: 'buffer' },
+		expected: 'idle',
+	},
+	{
+		current: 'idle',
+		operation: { type: 'complete' },
+		expected: 'idle',
+	},
+	// pending
+	{
+		current: 'pending',
+		operation: { type: 'fetch' },
+		expected: 'pending',
+	},
+	{
+		current: 'pending',
+		operation: { type: 'cancel' },
+		expected: 'cancelled',
+	},
+	{
+		current: 'pending',
+		operation: { type: 'reject' },
+		expected: 'failed',
+	},
+	{
+		current: 'pending',
+		operation: { type: 'buffer' },
+		expected: 'streaming',
+	},
+	{
+		current: 'pending',
+		operation: { type: 'complete' },
+		expected: 'completed',
+	},
+	// failed
+	{
+		current: 'failed',
+		operation: { type: 'fetch' },
+		expected: 'pending',
+	},
+	{
+		current: 'failed',
+		operation: { type: 'cancel' },
+		expected: 'failed',
+	},
+	{
+		current: 'failed',
+		operation: { type: 'reject' },
+		expected: 'failed',
+	},
+	{
+		current: 'failed',
+		operation: { type: 'buffer' },
+		expected: 'failed',
+	},
+	{
+		current: 'failed',
+		operation: { type: 'complete' },
+		expected: 'failed',
+	},
+	// streaming
+	{
+		current: 'streaming',
+		operation: { type: 'fetch' },
+		expected: 'streaming',
+	},
+	{
+		current: 'streaming',
+		operation: { type: 'cancel' },
+		expected: 'cancelled',
+	},
+	{
+		current: 'streaming',
+		operation: { type: 'reject' },
+		expected: 'failed',
+	},
+	{
+		current: 'streaming',
+		operation: { type: 'buffer' },
+		expected: 'streaming',
+	},
+	{
+		current: 'streaming',
+		operation: { type: 'complete' },
+		expected: 'completed',
+	},
+	// cancelled
+	{
+		current: 'cancelled',
+		operation: { type: 'fetch' },
+		expected: 'pending',
+	},
+	{
+		current: 'cancelled',
+		operation: { type: 'cancel' },
+		expected: 'cancelled',
+	},
+	{
+		current: 'cancelled',
+		operation: { type: 'reject' },
+		expected: 'cancelled',
+	},
+	{
+		current: 'cancelled',
+		operation: { type: 'buffer' },
+		expected: 'cancelled',
+	},
+	{
+		current: 'cancelled',
+		operation: { type: 'complete' },
+		expected: 'cancelled',
+	},
+	// completed
+	{
+		current: 'completed',
+		operation: { type: 'fetch' },
+		expected: 'pending',
+	},
+	{
+		current: 'completed',
+		operation: { type: 'cancel' },
+		expected: 'completed',
+	},
+	{
+		current: 'completed',
+		operation: { type: 'reject' },
+		expected: 'completed',
+	},
+	{
+		current: 'completed',
+		operation: { type: 'buffer' },
+		expected: 'completed',
+	},
+	{
+		current: 'completed',
+		operation: { type: 'complete' },
+		expected: 'completed',
+	},
+];
+
+test('transition', () => {
+	for (const t of TESTS) {
+		// @ts-ignore
+		expect(transition(t.current, t.operation)).toEqual(t.expected);
+	}
+});
---
+++ src/internals/state.ts
@@ -0,0 +1,64 @@
+// Ours
+import { Operation } from './operations';
+
+export type State =
+	| 'idle'
+	| 'pending'
+	| 'failed'
+	| 'streaming'
+	| 'cancelled'
+	| 'completed';
+
+/**
+ * Determines the next state given the current `state` and `operation`.
+ *
+ * Inspired by https://xstate.js.org/
+ *
+ * @param state
+ * @param operation
+ */
+export const transition = (
+	state: State,
+	operation: Operation
+): State => {
+	const event = operation.type;
+
+	switch (state) {
+		// Expecting:
+		// - fetch
+		case 'idle':
+		case 'failed':
+		case 'cancelled':
+		case 'completed': {
+			if (event === 'fetch') {
+				return 'pending';
+			}
+
+			break;
+		}
+
+		// Expecting:
+		// - buffer
+		// - reject
+		// - cancel
+		// - complete
+		case 'pending':
+		case 'streaming': {
+			switch (event) {
+				case 'buffer':
+					return 'streaming';
+				case 'reject':
+					return 'failed';
+				case 'cancel':
+					return 'cancelled';
+				case 'complete':
+					return 'completed';
+			}
+
+			break;
+		}
+	}
+
+	// fallback to current state
+	return state;
+};
